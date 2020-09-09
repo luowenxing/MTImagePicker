8f4e87d89479de8d4208340428fe49466f7d40f9
罗文兴 <511352272@qq.com>
2020-09-09 21:25:17 +0800

    Update MTImagePickerModel.swift

--- MTImagePicker/MTImagePicker/Model/MTImagePickerModel.swift
+++ MTImagePicker/MTImagePicker/Model/MTImagePickerModel.swift
@@ -1,11 +1,3 @@
-//
-//  ImageSelectorViewModel.swift
-//  CMBMobile
-//
-//  Created by Luo on 5/9/16.
-//  Copyright © 2016 Yst－WHB. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
